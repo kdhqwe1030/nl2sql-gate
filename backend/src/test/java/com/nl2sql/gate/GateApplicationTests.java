6827adbc8aabeb773630965c68f574948bda9587
kdhqwe1030 <kdhqwe1030@naver.com>
2026-09-08 00:24:27 +0900

    [back]init: 기초 패키지 구축

---
+++ backend/src/test/java/com/nl2sql/gate/GateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nl2sql.gate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
